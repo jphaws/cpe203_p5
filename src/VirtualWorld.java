b7a6c8f15d10291fa52ce91b7d6c6b102f141f8f
jaxon <jphaws@calpoly.edu>
2020-03-10 15:47:11 -0700

    Created rudimentary start screen and method to switch between levels

--- src/VirtualWorld.java
+++ src/VirtualWorld.java
@@ -11,130 +11,150 @@ current view (think virtual camera) into that world (WorldView)
  */
 
 public final class VirtualWorld
-   extends PApplet
-{
+   extends PApplet {
    private static final int TIMER_ACTION_PERIOD = 100;
 
    private static final int VIEW_WIDTH = 1280;
    private static final int VIEW_HEIGHT = 960;
-//   private static final int VIEW_WIDTH = 640;
+   //   private static final int VIEW_WIDTH = 640;
 //   private static final int VIEW_HEIGHT = 480;
-   private static final int TILE_WIDTH = 32;
-   private static final int TILE_HEIGHT = 32;
-   private static final int WORLD_WIDTH_SCALE = 2;
-   private static final int WORLD_HEIGHT_SCALE = 2;
+    private static final int TILE_WIDTH = 32;
+    private static final int TILE_HEIGHT = 32;
+    private static final int WORLD_WIDTH_SCALE = 2;
+    private static final int WORLD_HEIGHT_SCALE = 2;
 
-   private static final int VIEW_COLS = VIEW_WIDTH / TILE_WIDTH;
-   private static final int VIEW_ROWS = VIEW_HEIGHT / TILE_HEIGHT;
-   private static final int WORLD_COLS = VIEW_COLS * WORLD_WIDTH_SCALE;
-   private static final int WORLD_ROWS = VIEW_ROWS * WORLD_HEIGHT_SCALE;
+    private static final int VIEW_COLS = VIEW_WIDTH / TILE_WIDTH;
+    private static final int VIEW_ROWS = VIEW_HEIGHT / TILE_HEIGHT;
+    private static final int WORLD_COLS = VIEW_COLS * WORLD_WIDTH_SCALE;
+    private static final int WORLD_ROWS = VIEW_ROWS * WORLD_HEIGHT_SCALE;
 
-   private static final String IMAGE_LIST_FILE_NAME = "imagelist";
-   private static final String DEFAULT_IMAGE_NAME = "background_default";
-   private static final int DEFAULT_IMAGE_COLOR = 0x808080;
+    private static final String IMAGE_LIST_FILE_NAME = "imagelist";
+    private static final String DEFAULT_IMAGE_NAME = "background_default";
+    private static final int DEFAULT_IMAGE_COLOR = 0x808080;
 
-   private static final String LOAD_FILE_NAME = "world2.sav";
+   private String LOAD_FILE_NAME = "start.sav";
 
-   private static final String FAST_FLAG = "-fast";
-   private static final String FASTER_FLAG = "-faster";
-   private static final String FASTEST_FLAG = "-fastest";
-   private static final double FAST_SCALE = 0.5;
-   private static final double FASTER_SCALE = 0.25;
-   private static final double FASTEST_SCALE = 0.10;
+    private static final String FAST_FLAG = "-fast";
+    private static final String FASTER_FLAG = "-faster";
+    private static final String FASTEST_FLAG = "-fastest";
+    private static final double FAST_SCALE = 0.5;
+    private static final double FASTER_SCALE = 0.25;
+    private static final double FASTEST_SCALE = 0.10;
 
    private static double timeScale = 1.0;
    private String devMode = "";
    private boolean devModeLock = true;
+   private int levelNumber = -1;
+   private boolean levelCompleted = false;
 
-   private ImageStore imageStore;
-   private WorldModel world;
-   private WorldView view;
-   private EventScheduler scheduler;
-   private Player player;
-   private long next_time;
+    private ImageStore imageStore;
+    private WorldModel world;
+    private WorldView view;
+    private EventScheduler scheduler;
+    private Player player;
+    private long next_time;
 
-   public void settings()
-   {
+   public void settings() {
       size(VIEW_WIDTH, VIEW_HEIGHT);
    }
 
    /*
       Processing entry point for "sketch" setup.
    */
-   public void setup()
-   {
+   public void setup() {
       this.imageStore = new ImageStore(
-         createImageColored(TILE_WIDTH, TILE_HEIGHT, DEFAULT_IMAGE_COLOR));
+              createImageColored(TILE_WIDTH, TILE_HEIGHT, DEFAULT_IMAGE_COLOR));
       this.world = new WorldModel(WORLD_ROWS, WORLD_COLS,
-         createDefaultBackground(imageStore));
+              createDefaultBackground(imageStore));
       this.view = new WorldView(VIEW_ROWS, VIEW_COLS, this, world,
-         TILE_WIDTH, TILE_HEIGHT);
+              TILE_WIDTH, TILE_HEIGHT);
       this.scheduler = new EventScheduler(timeScale);
 
-      loadImages(IMAGE_LIST_FILE_NAME, imageStore, this);
-      loadWorld(world, LOAD_FILE_NAME, imageStore);
+        loadImages(IMAGE_LIST_FILE_NAME, imageStore, this);
+        loadWorld(world, LOAD_FILE_NAME, imageStore);
 
-      this.player = new Player("player", new Point(5, 5), 5, 5, imageStore.getImageList("player"), 0);
-      world.addEntity(player);
+        this.player = new Player("player", new Point(5, 5), 5, 5, imageStore.getImageList("player"), 0);
+        world.addEntity(player);
 
-      scheduleActions(world, scheduler, imageStore);
+        scheduleActions(world, scheduler, imageStore);
 
-      next_time = System.currentTimeMillis() + TIMER_ACTION_PERIOD;
-   }
+        next_time = System.currentTimeMillis() + TIMER_ACTION_PERIOD;
+    }
 
-   public void draw()
-   {
+   public void draw() {
+      if (levelCompleted) {
+         switch (levelNumber) {
+            case -1:
+               LOAD_FILE_NAME = "start.sav";
+               setup();
+               break;
+            case 0:
+               LOAD_FILE_NAME = "world.sav";
+               setup();
+               break;
+            case 1:
+               LOAD_FILE_NAME = "world2.sav";
+               setup();
+               break;
+            case 2:
+               LOAD_FILE_NAME = "world3.sav";
+               setup();
+               break;
+         }
+         levelCompleted = false;
+      }
       long time = System.currentTimeMillis();
-      if (time >= next_time)
-      {
+      if (time >= next_time) {
          this.scheduler.updateOnTime(time);
          next_time = time + TIMER_ACTION_PERIOD;
       }
       view.drawViewport();
    }
 
-   public void keyPressed() {
-      if (key == CODED) {
-         int dx = 0;
-         int dy = 0;
+    public void keyPressed() {
+        if (key == CODED) {
+            int dx = 0;
+            int dy = 0;
 
-         switch (keyCode) {
-            case UP:
-               dy = -1;
-               player.turn(1);
-               break;
-            case DOWN:
-               dy = 1;
-               player.turn(0);
-               break;
-            case LEFT:
-               dx = -1;
-               player.turn(2);
-               break;
-            case RIGHT:
-               dx = 1;
-               player.turn(3);
-               break;
-         }
-         Point pt = new Point(player.getPosition().x + dx, player.getPosition().y + dy);
-         player.setImageIndex(0);
-         player.scheduleActions(scheduler, world, imageStore);
-         if (!world.isOccupied(pt)) {
-            world.moveEntity(player, pt);
+            switch (keyCode) {
+                case UP:
+                    dy = -1;
+                    player.turn(1);
+                    break;
+                case DOWN:
+                    dy = 1;
+                    player.turn(0);
+                    break;
+                case LEFT:
+                    dx = -1;
+                    player.turn(2);
+                    break;
+                case RIGHT:
+                    dx = 1;
+                    player.turn(3);
+                    break;
+            }
+            Point pt = new Point(player.getPosition().x + dx, player.getPosition().y + dy);
+            player.setImageIndex(0);
+            player.scheduleActions(scheduler, world, imageStore);
+            if (!world.isOccupied(pt)) {
+                world.moveEntity(player, pt);
 //            view.shiftView(dx, dy);
-         }
-         else if(world.getOccupancyCell(pt) instanceof Gold) {
-             world.removeEntity(world.getOccupancyCell(pt));
-             world.moveEntity(player, pt);
-             player.increaseGoldCount();
-             System.out.println(player.getGoldCount());
          }
       }
       if(key == ' '){
             player.useWeapon(world, imageStore, scheduler);
       }
-      if(key == '~')
-         devModeLock = false;
+
+      if(key == ENTER || key == RETURN) {
+         if (levelNumber == -1) {
+            levelCompleted = true;
+            levelNumber++;
+         }
+      }
+         if(key == '~'){
+          devModeLock = false;
+      }
       if(!devModeLock){
          switch (key) {
             case 'o':
@@ -163,130 +183,130 @@ public final class VirtualWorld
       }
    }
 
-   public void mousePressed()
-{
-   Point pressed = mouseToPoint(mouseX, mouseY);
-   System.out.println("(" + pressed.x + ", " + pressed.y + ")");
+    public void mousePressed()
+    {
+        Point pressed = mouseToPoint(mouseX, mouseY);
+        System.out.println("(" + pressed.x + ", " + pressed.y + ")");
 
-   switch(devMode){
-      case "o":
-         if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Obstacle)
-          world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
-         else{
-            world.addEntity(new Obstacle("obstacle", new Point(pressed.x, pressed.y), imageStore.getImageList("obstacle")));
-         }
-         break;
-      case "a":
-         if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Atlantis)
-            world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
-         else{
-            world.addEntity(new Atlantis("atlantis", new Point(pressed.x, pressed.y), imageStore.getImageList("atlantis"), 0, 0));
-         }
-         break;
-      case "s":
-         if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Skeleton)
-            world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
-         else{
-            world.addEntity(new Skeleton("skeleton", new Point(pressed.x, pressed.y), imageStore.getImageList("skeleton"), 900, 100));
-         }
-         break;
-      case "g":
-         if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Gold)
-            world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
-         else{
-            world.addEntity(new Gold("gold", new Point(pressed.x, pressed.y), imageStore.getImageList("gold"), 100));
-         }
-         break;
-   }
+        switch(devMode){
+            case "o":
+                if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Obstacle)
+                    world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
+                else{
+                    world.addEntity(new Obstacle("obstacle", new Point(pressed.x, pressed.y), imageStore.getImageList("obstacle")));
+                }
+                break;
+            case "a":
+                if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Atlantis)
+                    world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
+                else{
+                    world.addEntity(new Atlantis("atlantis", new Point(pressed.x, pressed.y), imageStore.getImageList("atlantis"), 0, 0));
+                }
+                break;
+            case "s":
+                if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Skeleton)
+                    world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
+                else{
+                    world.addEntity(new Skeleton("skeleton", new Point(pressed.x, pressed.y), imageStore.getImageList("skeleton"), 900, 100));
+                }
+                break;
+            case "g":
+                if (world.getOccupancyCell(new Point(pressed.x, pressed.y)) instanceof Gold)
+                    world.removeEntity(world.getOccupancyCell(new Point(pressed.x, pressed.y)));
+                else{
+                    world.addEntity(new Gold("gold", new Point(pressed.x, pressed.y), imageStore.getImageList("gold"), 100));
+                }
+                break;
+        }
 
-   redraw();
+        redraw();
 
-}
-   private Point mouseToPoint(int x, int y)
-   {
-      return new Point(mouseX/32, mouseY/32);
-   }
+    }
+    private Point mouseToPoint(int x, int y)
+    {
+        return new Point(mouseX/32, mouseY/32);
+    }
 
 
-   public static Background createDefaultBackground(ImageStore imageStore)
-   {
-      return new Background(DEFAULT_IMAGE_NAME,
-         imageStore.getImageList(DEFAULT_IMAGE_NAME));
-   }
+    public static Background createDefaultBackground(ImageStore imageStore)
+    {
+        return new Background(DEFAULT_IMAGE_NAME,
+                imageStore.getImageList(DEFAULT_IMAGE_NAME));
+    }
 
-   public static PImage createImageColored(int width, int height, int color)
-   {
-      PImage img = new PImage(width, height, RGB);
-      img.loadPixels();
-      for (int i = 0; i < img.pixels.length; i++)
-      {
-         img.pixels[i] = color;
-      }
-      img.updatePixels();
-      return img;
-   }
+    public static PImage createImageColored(int width, int height, int color)
+    {
+        PImage img = new PImage(width, height, RGB);
+        img.loadPixels();
+        for (int i = 0; i < img.pixels.length; i++)
+        {
+            img.pixels[i] = color;
+        }
+        img.updatePixels();
+        return img;
+    }
 
-   private static void loadImages(String filename, ImageStore imageStore,
-      PApplet screen)
-   {
-      try
-      {
-         Scanner in = new Scanner(new File(filename));
-         imageStore.loadImages(in, screen);
-      }
-      catch (FileNotFoundException e)
-      {
-         System.err.println(e.getMessage());
-      }
-   }
+    private static void loadImages(String filename, ImageStore imageStore,
+                                   PApplet screen)
+    {
+        try
+        {
+            Scanner in = new Scanner(new File(filename));
+            imageStore.loadImages(in, screen);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
 
-   public static void loadWorld(WorldModel world, String filename,
-      ImageStore imageStore)
-   {
-      try
-      {
-         Scanner in = new Scanner(new File(filename));
-         world.load(in, imageStore);
-      }
-      catch (FileNotFoundException e)
-      {
-         System.err.println(e.getMessage());
-      }
-   }
+    public static void loadWorld(WorldModel world, String filename,
+                                 ImageStore imageStore)
+    {
+        try
+        {
+            Scanner in = new Scanner(new File(filename));
+            world.load(in, imageStore);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
 
-   public static void scheduleActions(WorldModel world,
-      EventScheduler scheduler, ImageStore imageStore)
-   {
-      for (AbstractEntity entity : world.getEntities())
-      {
-         //Only start actions for entities that include action (not those with just animations)
-         if (entity instanceof AbstractActiveEntity)  //entity instanceof Execuable
-            ((AbstractActiveEntity)entity).scheduleActions(scheduler, world, imageStore);
-      }
-   }
+    public static void scheduleActions(WorldModel world,
+                                       EventScheduler scheduler, ImageStore imageStore)
+    {
+        for (AbstractEntity entity : world.getEntities())
+        {
+            //Only start actions for entities that include action (not those with just animations)
+            if (entity instanceof AbstractActiveEntity)  //entity instanceof Execuable
+                ((AbstractActiveEntity)entity).scheduleActions(scheduler, world, imageStore);
+        }
+    }
 
-   public static void parseCommandLine(String [] args)
-   {
-      for (String arg : args)
-      {
-         switch (arg)
-         {
-            case FAST_FLAG:
-               timeScale = Math.min(FAST_SCALE, timeScale);
-               break;
-            case FASTER_FLAG:
-               timeScale = Math.min(FASTER_SCALE, timeScale);
-               break;
-            case FASTEST_FLAG:
-               timeScale = Math.min(FASTEST_SCALE, timeScale);
-               break;
-         }
-      }
-   }
+    public static void parseCommandLine(String [] args)
+    {
+        for (String arg : args)
+        {
+            switch (arg)
+            {
+                case FAST_FLAG:
+                    timeScale = Math.min(FAST_SCALE, timeScale);
+                    break;
+                case FASTER_FLAG:
+                    timeScale = Math.min(FASTER_SCALE, timeScale);
+                    break;
+                case FASTEST_FLAG:
+                    timeScale = Math.min(FASTEST_SCALE, timeScale);
+                    break;
+            }
+        }
+    }
 
-   public static void main(String [] args)
-   {
-      parseCommandLine(args);
-      PApplet.main(VirtualWorld.class);
-   }
+    public static void main(String [] args)
+    {
+        parseCommandLine(args);
+        PApplet.main(VirtualWorld.class);
+    }
 }
